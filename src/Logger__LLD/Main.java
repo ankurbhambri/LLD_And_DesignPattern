8facffd176fb00513228cd77b66a6a19bf2d0435
Ankur Bhambri <ankurbhambri@microsoft.com>
2024-11-09 20:14:37 +0530

    new code

--- src/Logger__LLD/Main.java
+++ src/Logger__LLD/Main.java
@@ -2,6 +2,6 @@ package Logger__LLD;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Logger LLD");
+        System.out.println("Logger LLD!");
     }
 }
